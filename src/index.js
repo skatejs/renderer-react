17d65bae83617f3ea964f1678b4ee512ec2e8914
Trey Shugart <treshugart@gmail.com>
2017-09-11 23:48:23 +1000

    Remove hard dependency on Skate.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { withProps, withRenderer } from 'skatejs/esnext';
 
 export const withReact = Base =>
-  class extends withRenderer(withProps(Base || HTMLElement)) {
+  class extends (Base || HTMLElement) {
     get props() {
       // We override props so that we can satisfy most use
       // cases for children by using a slot.
