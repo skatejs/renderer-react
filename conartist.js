4d808b26dca25ca2dbb3bc08676f64cfbadec676
Trey Shugart <treshugart@gmail.com>
2017-09-26 10:15:15 +1000

    Update conartist file.

--- conartist.js
+++ conartist.js
@@ -2,7 +2,7 @@ const { config, preset } = require('conartist');
 
 module.exports = config(preset.babel(), preset.base(), preset.jest(), preset.rollup(), {
   'rollup.config.js'() {
-    return Object.assign({}, rollup()['rollup.config.js'](), {
+    return Object.assign({}, preset.rollup()['rollup.config.js'](), {
       globals: {
         react: 'React',
         'react-dom': 'ReactDOM'
