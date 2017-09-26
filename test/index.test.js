0d56e97c56a6c1f8113790927cb3c51b91ede3b0
Trey Shugart <treshugart@gmail.com>
2017-09-26 10:57:48 +1000

    Add tests.

---
+++ test/index.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import withRenderer from '../src';
+
+class MyElement extends withRenderer() {
+  renderCallback({ name }) {
+    return <div>Hello, {name}!</div>;
+  }
+}
+customElements.define('my-element', MyElement);
+
+function testContent(text) {
+  return `<div data-reactroot=\"\"><!-- react-text: 2 -->Hello, <!-- /react-text --><!-- react-text: 3 -->${text}<!-- /react-text --><!-- react-text: 4 -->!<!-- /react-text --></div>`;
+}
+
+test('renders', () => {
+  const el = new MyElement();
+  expect(el.innerHTML).toEqual('');
+  el.rendererCallback(el, el.renderCallback.bind(el, { name: 'World' }));
+  expect(el.innerHTML).toEqual('<div>Hello, World!</div>');
+  el.rendererCallback(el, el.renderCallback.bind(el, { name: 'Bob' }));
+  expect(el.innerHTML).toEqual('<div>Hello, Bob!</div>');
+});
