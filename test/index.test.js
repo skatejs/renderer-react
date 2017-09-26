c9585fd2e712aa5e58c3329c2b36faceba8348d3
Trey Shugart <treshugart@gmail.com>
2017-09-26 11:23:45 +1000

    Add test for wrapping a react component.

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import withRenderer from '../src';
 
 class MyElement extends withRenderer() {
@@ -8,15 +8,42 @@ class MyElement extends withRenderer() {
 }
 customElements.define('my-element', MyElement);
 
-function testContent(text) {
-  return `<div data-reactroot=\"\"><!-- react-text: 2 -->Hello, <!-- /react-text --><!-- react-text: 3 -->${text}<!-- /react-text --><!-- react-text: 4 -->!<!-- /react-text --></div>`;
-}
-
 test('renders', () => {
+  function testContent(text) {
+    return `<div data-reactroot=\"\"><!-- react-text: 2 -->Hello, <!-- /react-text --><!-- react-text: 3 -->${text}<!-- /react-text --><!-- react-text: 4 -->!<!-- /react-text --></div>`;
+  }
+
   const el = new MyElement();
   expect(el.innerHTML).toEqual('');
   el.rendererCallback(el, el.renderCallback.bind(el, { name: 'World' }));
   expect(el.innerHTML).toEqual(testContent('World'));
   el.rendererCallback(el, el.renderCallback.bind(el, { name: 'Bob' }));
   expect(el.innerHTML).toEqual(testContent('Bob'));
+});
+
+test('wrappers', () => {
+  class ReactComponent extends Component {
+    render() {
+      return <div>Hello, {this.props.children}!</div>;
+    }
+  }
+
+  class ReactComponentWrapper extends withRenderer() {
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+    }
+    renderCallback() {
+      return <ReactComponent {...this.props} />;
+    }
+  }
+
+  customElements.define('react-component-wrapper', ReactComponentWrapper);
+
+  const el = new ReactComponentWrapper();
+  const { shadowRoot } = el;
+  el.rendererCallback(shadowRoot, el.renderCallback.bind(el));
+  expect(shadowRoot.innerHTML).toEqual(
+    '<div data-reactroot=""><!-- react-text: 2 -->Hello, <!-- /react-text --><slot></slot><!-- react-text: 4 -->!<!-- /react-text --></div>'
+  );
 });
